99f90698dba00dc509012e5eb40374477832a758
Adam C. Baker <adam.durandal@gmail.com>
2013-10-30 21:36:49 -0700

    getToday to get today's date

--- src/Constellation/Github.hs
+++ src/Constellation/Github.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Constellation.Github(
   getUserRepo,
   starsFrom,
+  getToday,
 ) where
 
-import Prelude
 import Control.Monad.IO.Class (MonadIO)
 import qualified Data.ByteString.Lazy as BSL
 import Network.HTTP.Conduit
 {-import Constellation.Models.Github-}
 import Data.Aeson(decode, (.:))
 import Data.Aeson.Types (parseMaybe)
+import Data.Time.Clock(getCurrentTime, utctDay)
+import Data.Time.Calendar(Day)
 
 type UserName = String
 type RepoName = String
@@ -18,6 +21,9 @@ type JSON = BSL.ByteString
 getUserRepo :: MonadIO m => UserName -> RepoName -> m JSON
 getUserRepo uname rname = simpleHttp url
   where url = "https://api.github.com/repos/"++uname++"/"++rname
+
+getToday :: IO Day
+getToday = return . utctDay =<< getCurrentTime
 
 starsFrom :: JSON -> Maybe Int
 starsFrom repo = flip parseMaybe obj $ (.: "watchers")
